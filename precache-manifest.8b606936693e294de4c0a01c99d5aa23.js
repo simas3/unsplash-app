73b87b48c1a91705e844923f0523ce3a6bc87b93
Ssimas3 <sstasytis@gmail.com>
2018-11-19 22:14:15 +0200

    Updates

---
+++ precache-manifest.8b606936693e294de4c0a01c99d5aa23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89c8ab2b4626670dd2d",
+    "url": "/unsplash-app/static/js/runtime~main.c89c8ab2.js"
+  },
+  {
+    "revision": "a520e652ccb9b3675878",
+    "url": "/unsplash-app/static/js/main.a520e652.chunk.js"
+  },
+  {
+    "revision": "080d53f89dc164e5da80",
+    "url": "/unsplash-app/static/js/1.080d53f8.chunk.js"
+  },
+  {
+    "revision": "a520e652ccb9b3675878",
+    "url": "/unsplash-app/static/css/main.3878ef0a.chunk.css"
+  },
+  {
+    "revision": "080d53f89dc164e5da80",
+    "url": "/unsplash-app/static/css/1.a12acf1e.chunk.css"
+  },
+  {
+    "revision": "c43b5ba911671dc2f237274e8ab81afa",
+    "url": "/unsplash-app/index.html"
+  }
+];
